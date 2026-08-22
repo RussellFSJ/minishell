a89e8c2126055936405b36229fe146ab501a7aa3
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-22 16:23:18 +0800

    feat(env): implement env_new and env_add_back

--- src/env/env_utils.c
+++ src/env/env_utils.c
@@ -6,22 +6,52 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:14:45 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/06 14:50:57 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/22 16:21:51 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 
+#include "env.h"
+#include "libft.h"
+
 t_env	*env_new(char *key, char *value, int exported)
 {
-	(void)key;
-	(void)value;
-	(void)exported;
-	return (NULL);
+	t_env	*node;
+
+	node = malloc(sizeof(t_env));
+	if (!node)
+		return (NULL);
+	node->key = ft_strdup(key);
+	node->value = NULL;
+	if (value)
+		node->value = ft_strdup(value);
+	if (!node->key || (value && !node->value))
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (NULL);
+	}
+	node->exported = exported;
+	node->next = NULL;
+	return (node);
 }
 
 void	env_add_back(t_env **env, t_env *node)
 {
-	(void)env;
-	(void)node;
+	t_env	*current;
+
+	if (!env || !node)
+		return ;
+	if (!*env)
+	{
+		*env = node;
+		return ;
+	}
+	current = *env;
+	while (current->next)
+		current = current->next;
+	current->next = node;
 }
+
