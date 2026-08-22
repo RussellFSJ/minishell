1792048bcd69b92627a4ad1ed04f9ed4b8874a80
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-22 16:52:06 +0800

    feat(env): implement environment array conversion

--- src/env/env_array.c
+++ src/env/env_array.c
@@ -6,14 +6,75 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:14:36 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/06 14:50:33 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/22 16:49:52 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
+#include "libft.h"
+#include <stdlib.h>
+
+static size_t	env_count_exported(t_env *env)
+{
+	size_t	count;
+
+	count = 0;
+	while (env)
+	{
+		if (env->exported && env->value)
+			count++;
+		env = env->next;
+	}
+	return (count);
+}
+
+static char	*env_make_entry(t_env *env)
+{
+	char	*prefix;
+	char	*entry;
+
+	prefix = ft_strjoin(env->key, "=");
+	if (!prefix)
+		return (NULL);
+	entry = ft_strjoin(prefix, env->value);
+	free(prefix);
+	return (entry);
+}
+
+static char	**env_free_partial(char **array, size_t count)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+	return (NULL);
+}
 
 char	**env_to_array(t_env *env)
 {
-	(void)env;
-	return (NULL);
+	char	**array;
+	size_t	i;
+
+	array = malloc(sizeof(char *) * (env_count_exported(env) + 1));
+	if (!array)
+		return (NULL);
+	i = 0;
+	while (env)
+	{
+		if (env->exported && env->value)
+		{
+			array[i] = env_make_entry(env);
+			if (!array[i])
+				return (env_free_partial(array, i));
+			i++;
+		}
+		env = env->next;
+	}
+	array[i] = NULL;
+	return (array);
 }
