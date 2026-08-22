0ea6650857bccc7db37abc904465e947a09e6386
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-22 16:55:08 +0800

    feat(cleanup): implement environment cleanup

--- src/cleanup/free_env.c
+++ src/cleanup/free_env.c
@@ -6,19 +6,39 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:14:30 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/06 14:53:13 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/22 16:54:19 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cleanup.h"
 #include "env.h"
+#include <stdlib.h>
 
 void	free_env(t_env *env)
 {
-	(void)env;
+	t_env	*next;
+
+	while (env)
+	{
+		next = env->next;
+		free(env->key);
+		free(env->value);
+		free(env);
+		env = next;
+	}
 }
 
 void	free_array(char **array)
 {
-	(void)array;
+	size_t	i;
+
+	if (!array)
+		return ;
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
 }
