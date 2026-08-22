bb8145b187514a33c0a275254e4c718c7836e6f0
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-22 16:34:28 +0800

    feat(env): implement environment initialisation

--- src/env/env_init.c
+++ src/env/env_init.c
@@ -6,14 +6,49 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:14:43 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/06 14:50:51 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/22 16:33:00 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
+#include "libft.h"
+
+static int	env_add_entry(t_env **env, char *entry)
+{
+	char	*equal;
+	char	*key;
+	t_env	*node;
+
+	equal = ft_strchr(entry, '=');
+	if (!equal)
+		return (0);
+	key = ft_substr(entry, 0, equal - entry);
+	if (!key)
+		return (-1);
+	node = env_new(key, equal + 1, 1);
+	free(key);
+	if (!node)
+		return (-1);
+	env_add_back(env, node);
+	return (0);
+}
 
 t_env	*env_init(char **envp)
 {
-	(void)envp;
-	return (NULL);
+	t_env	*env;
+	int		i;
+
+	env = NULL;
+	i = 0;
+	while (envp && envp[i])
+	{
+		if (env_add_entry(&env, envp[i]) == -1)
+		{
+			free_env(env);
+			return (NULL);
+		}
+		i++;
+	}
+	return (env);
 }
+
