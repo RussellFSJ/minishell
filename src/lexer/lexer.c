ec5f8dbc5c3aa8a2b028417e3a533b76da51e621
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-23 04:30:19 +0800

    feat(lexer): implement main tokenization loop

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:15:18 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/23 01:50:58 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/23 04:29:10 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,25 @@ static void skip_spaces(const char *line, int *pos)
 
 t_token	*lex(const char *line, int *error)
 {
-	(void)line;
+	t_token	*list;
+	int		pos;
+
+	list = NULL;
+	pos = 0;
 	*error = 0;
-	return (NULL);
+	while (line[pos])
+	{
+		skip_spaces(line, &pos);
+		if (line[pos] == '\0')
+			break ;
+		if (is_operator_start(line[pos]))
+			*error = lex_operator(line, &pos, &list);
+		else
+			*error = lex_word(line, &pos, &list);
+		if (*error == -1)
+			return (list);
+	}
+	return (list);
 }
 
 t_token	*token_new(t_token_type type, char *value)
