db7d7ae4b8583cb5100eca7eca8aff00b48b6d08
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-23 04:40:28 +0800

    test(lexer): add token dump and lexer validation

--- src/lexer/lexer_dump.c
+++ src/lexer/lexer_dump.c
@@ -6,14 +6,39 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:15:10 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/06 14:51:20 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/23 04:38:46 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
+#include "libft.h"
+
+static char	*token_type_name(t_token_type type)
+{
+	if (type == TOKEN_WORD)
+		return ("WORD");
+	if (type == TOKEN_PIPE)
+		return ("PIPE");
+	if (type == TOKEN_REDIR_IN)
+		return ("REDIR_IN");
+	if (type == TOKEN_REDIR_OUT)
+		return ("REDIR_OUT");
+	if (type == TOKEN_REDIR_APPEND)
+		return ("REDIR_APPEND");
+	if (type == TOKEN_HEREDOC)
+		return ("HEREDOC");
+	return ("UNKNOWN");
+}
 
 void	dump_tokens(t_token *tokens, int fd)
 {
-	(void)tokens;
-	(void)fd;
+	while (tokens != NULL)
+	{
+		ft_putstr_fd(token_type_name(tokens->type), fd);
+		ft_putstr_fd(": ", fd);
+		if (tokens->value != NULL)
+			ft_putstr_fd(tokens->value, fd);
+		ft_putchar_fd('\n', fd);
+		tokens = tokens->next;
+	}
 }
--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -6,26 +6,21 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:15:54 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/06 14:59:08 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/23 04:39:36 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "types.h"
 #include "lexer.h"
-#include "cleanup.h"
+#include <stdio.h>
 
-int	main(int argc, char **argv)
+int	main(void)
 {
 	t_token	*tokens;
 	int		error;
 
-	(void)argc;
-	(void)argv;
-	error = 0;
-	tokens = lex("echo hello", &error);
-	printf("lex -> tokens=%p error=%d\n", (void *)tokens, error);
+	tokens = lex("cat < infile | grep \"hello world\" >> outfile", &error);
+	printf("tokens=%p error=%d\n", (void *)tokens, error);
 	dump_tokens(tokens, 1);
-	free_tokens(tokens);
 	return (0);
 }
+
