d820658963fb40c0ad3547bccde420eb632aafa3
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-23 04:01:31 +0800

    feat(lexer): add word tokenization

--- src/lexer/lexer_quotes.c
+++ src/lexer/lexer_quotes.c
@@ -6,18 +6,49 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:15:16 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/23 03:27:03 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/23 03:41:15 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
+#include "libft.h"
+#include <stdlib.h>
+
+static int	add_word_token(t_token **list, char *value)
+{
+	t_token	*token;
+
+	token = token_new(TOKEN_WORD, value);
+	if (token == NULL)
+	{
+		free(value);
+		return (-1);
+	}
+	token_add_back(list, token);
+	return (0);
+}
 
 int	lex_word(const char *line, int *pos, t_token **list)
 {
-	(void)line;
-	(void)pos;
-	(void)list;
-	return (0);
+	int		start;
+	char	*value;
+
+	start = *pos;
+	while (line[*pos] && !ft_isspace(line[*pos])
+		&& !is_operator_start(line[*pos]))
+	{
+		if (line[*pos] == '\'' || line[*pos] == '"')
+		{
+			if (quote_scan(line, pos) == -1)
+				return (-1);
+		}
+		else
+			(*pos)++;
+	}
+	value = ft_substr(line, start, *pos - start);
+	if (value == NULL)
+		return (-1);
+	return (add_word_token(list, value));
 }
 
 int	quote_scan(const char *line, int *pos)
