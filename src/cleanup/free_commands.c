7057fc167536163aa907c90182fc73d05c29086b
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-29 23:23:59 +0800

    feat(cleanup): implement command and redirection cleanup

--- src/cleanup/free_commands.c
+++ src/cleanup/free_commands.c
@@ -6,18 +6,35 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:14:27 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/06 14:53:06 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/29 23:21:06 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cleanup.h"
+#include "minishell.h"
 
 void	free_commands(t_command *cmds)
 {
-	(void)cmds;
+	t_command	*next;
+
+	while (cmds)
+	{
+		next = cmds->next;
+		free_array(cmds->argv);
+		free_redirs(cmds->redirs);
+		free(cmds);
+		cmds = next;
+	}
 }
 
 void	free_redirs(t_redir *redirs)
 {
-	(void)redirs;
+	t_redir	*next;
+
+	while (redirs)
+	{
+		next = redirs->next;
+		free(redirs->target);
+		free(redirs);
+		redirs = next;
+	}
 }
