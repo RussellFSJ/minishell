eddd7d89e8e4ae51f93b012558cb7e31d4d0402d
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-08-30 01:57:10 +0800

    feat(parser): implement main parsing flow

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,17 +6,71 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 12:15:26 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/08/29 23:33:51 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/08/30 01:54:10 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	build_redirs(t_command *cmd, t_token *tokens)
+{
+	while (tokens && tokens->type != TOKEN_PIPE)
+	{
+		if (tokens->type >= TOKEN_REDIR_IN
+			&& tokens->type <= TOKEN_HEREDOC)
+		{
+			if (parser_add_redir(cmd, tokens) == -1)
+				return (-1);
+		}
+		tokens = tokens->next;
+	}
+	return (0);
+}
+
+static int	build_command(t_command **list, t_token *tokens)
+{
+	t_command	*cmd;
+
+	cmd = command_new();
+	if (!cmd)
+		return (-1);
+	if (parser_build_argv(cmd, tokens) == -1
+		|| build_redirs(cmd, tokens) == -1)
+	{
+		free_commands(cmd);
+		return (-1);
+	}
+	command_add_back(list, cmd);
+	return (0);
+}
+
 t_command	*parse(t_token *tokens, int *error)
 {
-	(void)tokens;
+	t_command	*cmds;
+
+	if (!error)
+		return (NULL);
 	*error = 0;
-	return (NULL);
+	if (parser_check_syntax(tokens) == -1)
+	{
+		*error = -1;
+		return (NULL);
+	}
+	cmds = NULL;
+	while (tokens)
+	{
+		if (build_command(&cmds, tokens) == -1)
+		{
+			free_commands(cmds);
+			*error = -1;
+			return (NULL);
+		}
+		while (tokens && tokens->type != TOKEN_PIPE)
+			tokens = tokens->next;
+		if (tokens)
+			tokens = tokens->next;
+	}
+	return (cmds);
 }
 
 t_command	*command_new(void)
